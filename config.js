00ec7f8a3f21283dc34b996ca52feaef81cc3b2e
Yahya <82366608+Yahyax001@users.noreply.github.com>
2022-08-09 22:43:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: '',
+  token: 'OTk1MDYxODMzNzU5MTk1MjU2.G3tIeD.Z9On2cYAlIGLZMpqakexaS9B1oYzZzfbx3imCE',
   color: '#303136',
   globalBot: true, // false => if bot is private bot - true => if bot isn't private bot
   guildID: "Server ID",
