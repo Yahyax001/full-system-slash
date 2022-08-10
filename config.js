602039319bea4d188209b011c8852611ca427b2c
Yahya <82366608+Yahyax001@users.noreply.github.com>
2022-08-10 11:13:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: 'OTk1MDYxODMzNzU5MTk1MjU2.G3tIeD.Z9On2cYAlIGLZMpqakexaS9B1oYzZzfbx3imCE',
+  token: 'OTk1MDYxODMzNzU5MTk1MjU2.GAumCn.M2iCb9b4x8AwSaRiV6xroTT-QBBlt9aS4JWCBw',
   color: '#303136',
   globalBot: true, // false => if bot is private bot - true => if bot isn't private bot
   guildID: "Server ID",
